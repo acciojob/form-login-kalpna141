2de00789738e6b6a12dc80ca96d89d0a3de4ba77
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-06-20 11:28:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
+ var x=document.getElementById("form1");
+   for(var i=0; i<x.length; i++)
+   {
+    if(x.elements[i].value!='submit')
+    {
+       alert(x.elements[i].value+x.elements[i+1].value);
+    }
+   }
 
 }
